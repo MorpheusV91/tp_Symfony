acf7ad0e85d9ec6306f7c48bac581bceeea1d7f3
gibralta fabrice <gibralta.fabrice@gmx.fr>
2022-03-29 10:06:29 +0200

    creation du controller page d'accueil fonctionnelle

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,3 +1,19 @@
 <?php
 
-namespace App\controller;
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController {
+
+      /**
+      * @Route("/home", name = "accueil")
+      * @Route("/")
+      */
+      public function home(): Response {
+            return  $this->render('home.html.twig');
+      }
+    
+}
